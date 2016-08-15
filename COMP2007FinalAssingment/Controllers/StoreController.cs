6d3fa939870b811b45f93771c97bc29b8897214b
Phizzard <philip.tietjen@mygeorgian.ca>
2016-08-15 19:46:41 -0400

    Added categoreis into main store page, not functional yet cause im going to the gym. PEACE

---
+++ COMP2007FinalAssingment/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using COMP2007FinalAssingment.Models;
+
+namespace COMP2007FinalAssingment.Controllers
+{
+    public class StoreController : Controller
+    {
+        private AtlasStoreContext db = new AtlasStoreContext();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            ViewBag.Products = db.Products;
+            ViewBag.Brands = db.Brands;
+            ViewBag.Goals = db.Goals;
+            ViewBag.Ingredients = db.Ingredients;
+            
+            return View();
+        }
+    }
+}
