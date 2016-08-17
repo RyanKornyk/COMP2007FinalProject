578a4824769c3e95b622027346bc34c589532146
Phizzard <philip.tietjen@mygeorgian.ca>
2016-08-16 22:53:33 -0400

    Added gross limiter on front page products

--- COMP2007FinalAssingment/Controllers/HomeController.cs
+++ COMP2007FinalAssingment/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using COMP2007FinalAssingment.Models;
 
 namespace COMP2007FinalAssingment.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
+
+        private AtlasStoreContext db = new AtlasStoreContext();
+
         public ActionResult Index()
         {
+            ViewBag.Products = db.Products;
             return View();
         }
 
